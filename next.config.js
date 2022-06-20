2292792ffa4309ed464cc6f90bb78ae88c41c69e
krishnajhapate <ki54.jhapate@gmail.com>
2022-06-20 09:52:42 +0530

    graphql integrated with fatapi and next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/graphql",
+          destination: "http://localhost:8000/graphql",
+        },
+      ],
+    };
+  },
 }
 
 module.exports = nextConfig
